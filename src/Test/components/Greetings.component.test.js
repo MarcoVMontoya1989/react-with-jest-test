170a75427efed053454dcdd6df8b51a932a3ca2b
Marco Antonio V Montoya <marcovmontoya@gmail.com>
2021-10-20 22:04:18 -0400

    feat: correction files

--- src/Test/components/Greetings.component.test.js
+++ src/Test/components/Greetings.component.test.js
@@ -1,21 +1,8 @@
 import {render, screen} from '@testing-library/react';
 import GreetingsComponent from "../../components/Greetings.component";
 
-// afterEach(() => {
-//   jest.useRealTimers();
-// });
-
-
-jest.setTimeout(6000);
-
 describe('Greetings component unit test', () => {
-
-  // jest.useFakeTimers('legacy');
-
-  it('should render Hello World as a text', (assert) => {
-    jest.useFakeTimers('legacy');
-
-
+  test('should render Hello World as a text', () => {
     render(<GreetingsComponent />);
 
     const helloWorldElement = screen.getByText('Hello World!');
--- src/components/Greetings.component.jsx
+++ src/components/Greetings.component.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const GreetingsComponent = () => {
+
+  const [change, setChange] = useState(false);
+
+  const changeTheState = () => {
+    setChange(true);
+  };
+
   return (
     <div>
       <h2>Hello World!</h2>
-      <p>It's good to see you</p>
+
+      {!change && <p>it's good to see you</p>}
+      {change && <p>Changed magically</p>}
+      <button onClick={changeTheState}></button>
     </div>
   );
 };
