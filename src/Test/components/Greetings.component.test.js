edb04aaf7aeb78ba2112b03f5d5ba9fbed25583f
Marco Antonio V Montoya <marcovmontoya@gmail.com>
2021-10-20 22:22:59 -0400

    feat: add more test

--- src/Test/components/Greetings.component.test.js
+++ src/Test/components/Greetings.component.test.js
@@ -1,5 +1,6 @@
 import {render, screen} from '@testing-library/react';
 import GreetingsComponent from "../../components/Greetings.component";
+import userEvent from "@testing-library/user-event";
 
 describe('Greetings component unit test', () => {
   test('should render Hello World as a text', () => {
@@ -8,5 +9,35 @@ describe('Greetings component unit test', () => {
     const helloWorldElement = screen.getByText('Hello World!');
 
     expect(helloWorldElement).toBeInTheDocument();
+  });
+  
+  test('renders good to see you if the button was not clicked', () => {
+    render(<GreetingsComponent />);
+
+    const outputElement = screen.getByText(`good to see you`, {exact: true});
+
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test('renders and change the text after the button is clicked', () => {
+    render(<GreetingsComponent />);
+
+    const buttonElement = screen.getByRole('button');
+
+    userEvent.click(buttonElement);
+
+    const outputElement = screen.getByText(`Changed magically`, {exact: true});
+    expect(outputElement).toBeInTheDocument();
+  });
+  
+  test('it does not render good to see you', () => {
+    render(<GreetingsComponent />);
+
+    const buttonElement = screen.getByRole('button');
+    userEvent.click(buttonElement);
+
+    const outputElement = screen.queryByText(`good to see you`, {exact: false});
+    // expect(outputElement).not.toBeInTheDocument();
+    expect(outputElement).toBeNull();
   });
 });
--- src/components/Greetings.component.jsx
+++ src/components/Greetings.component.jsx
@@ -12,9 +12,9 @@ const GreetingsComponent = () => {
     <div>
       <h2>Hello World!</h2>
 
-      {!change && <p>it's good to see you</p>}
+      {!change && <p>good to see you</p>}
       {change && <p>Changed magically</p>}
-      <button onClick={changeTheState}></button>
+      <button onClick={changeTheState}>Change it</button>
     </div>
   );
 };
